d24ca42295a972d4b840f61c13a1b8d9981cc3fd
unknown <sashashepelenko@gmail.com>
2018-09-24 22:31:35 +0300

    commit

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -11,10 +11,6 @@ import Styles from './styles.m.css';
 
 export default class Feed extends Component {
     render () {
-        const {
-            avatar,
-            currentUserFirstName
-        } = this.props;
         return (
             <section className = { Styles.feed }>
                 <StatusBar />
