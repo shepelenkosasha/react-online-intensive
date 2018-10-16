e788653684f82d2a38e921a370af799b2ba95a04
unknown <sashashepelenko@gmail.com>
2018-10-16 21:35:56 +0300

    Homework 10. Animated remote component Post

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,6 +1,6 @@
 // Core
 import React, { Component } from 'react';
-import { Transition } from 'react-transition-group';
+import { Transition, CSSTransition, TransitionGroup } from 'react-transition-group';
 import { fromTo } from 'gsap';
 
 // Components
@@ -159,13 +159,26 @@ export default class Feed extends Component {
 
         const postsJSX = posts.map((post) => {
             return (
-                <Catcher key = { post.id }>
-                    <Post
-                        { ...post }
-                        _likePost = { this._likePost }
-                        _removePost = { this._removePost }
-                    />
-                </Catcher>
+                <CSSTransition
+                    classNames = { {
+                        enter:       Styles.postInStart,
+                        enterActive: Styles.postInEnd,
+                        exit:        Styles.postOutStart,
+                        exitActive:  Styles.postOutEnd,
+                    } }
+                    key = { post.id }
+                    timeout = { {
+                        enter: 500,
+                        exit:  400,
+                    } }>
+                    <Catcher>
+                        <Post
+                            { ...post }
+                            _likePost = { this._likePost }
+                            _removePost = { this._removePost }
+                        />
+                    </Catcher>
+                </CSSTransition>
             );
         });
 
@@ -188,7 +201,7 @@ export default class Feed extends Component {
                     onEntered = { this._animatePostmanEntered }>
                     <Postman />
                 </Transition>
-                {postsJSX}
+                <TransitionGroup>{postsJSX}</TransitionGroup>
             </section>
         );
     }
